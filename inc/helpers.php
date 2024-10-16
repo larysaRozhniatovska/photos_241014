766547509f9444f8f13cbf1a4dc03d340ad5877b
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-16 10:29:25 +0300

    helpers init

--- inc/helpers.php
+++ inc/helpers.php
@@ -8,3 +8,34 @@ function getUrl(string $action) : string
 {
     return '/index.php?action=' . $action;
 }
+function init() : void
+{
+    // Reading the GET parameter 'action' using filter_input
+    $action = filter_input(INPUT_GET, 'action');
+
+    if(!$action){
+        notFound();
+    }
+
+    $action = htmlspecialchars($action);
+
+    // Checking if a function with the same name exists
+    if (!function_exists($action)) {
+        // Generating a 404 error
+        notFound();
+    }
+    // Если функция существует, вызываем ее
+    call_user_func($action);
+}
+
+
+/**
+ * @return never
+ */
+function notFound() : never
+{
+    //header("HTTP/1.0 404 Not Found");
+    http_response_code(404);
+    render('not_found');
+    exit();
+}
