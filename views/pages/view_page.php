9eb7e82b47563c863222dbc64d67f590018aff83
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-16 09:48:34 +0300

    add views/pages/view_page

---
+++ views/pages/view_page.php
@@ -0,0 +1,33 @@
+<?php
+?>
+
+<div class="pswp-gallery pswp-gallery--single-column" id="gallery--with-custom-button">
+    <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-2500.jpg"
+       data-pswp-width="1669"
+       data-pswp-height="2500"
+       target="_blank">
+        <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg" alt="" />
+    </a>
+    <!-- cropped thumbnail: -->
+    <a href="../images/image1.png"
+       data-pswp-width="1875"
+       data-pswp-height="2500"
+       data-cropped="true"
+       target="_blank">
+        <img src="../images/image1.png" alt="" />
+    </a>
+    <!-- data-pswp-src with custom URL in href -->
+    <a href="https://unsplash.com"
+       data-pswp-src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg"
+       data-pswp-width="2500"
+       data-pswp-height="1666"
+       target="_blank">
+        <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg" alt="" />
+    </a>
+    <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/6/img-2500.jpg"
+       data-pswp-width="2500"
+       data-pswp-height="1667"
+       target="_blank">
+        <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/6/img-200.jpg" alt="" />
+    </a>
+</div>
