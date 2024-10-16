9cf057526ca63c12b384091a80da3f83ff17ae84
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-16 19:10:57 +0300

    end

--- views/common/menu.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-        <?php foreach (PAGES as $page_item): ?>
-            <li><a href="<?= getUrl($page_item['action']); ?>"><?= $page_item['name']; ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-</nav>
