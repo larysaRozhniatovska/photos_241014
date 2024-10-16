d695419ad9961374b82002e5dabb7dd903da8b92
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-16 12:15:28 +0300

    form_page input errors

--- views/pages/form_page.php
+++ views/pages/form_page.php
@@ -1,6 +1,11 @@
-<?php
-?>
-
+<h1>Add File</h1>
+<?php if (count($errors) > 0) :?>
+    <ul>
+        <?php foreach ($errors as $error) :?>
+            <li><?= $error?></li>
+        <?php endforeach;?>
+    </ul>
+<?php endif;?>
 <form action="<?= getUrl('proc')?>" method="post"  enctype="multipart/form-data" >
     <label for="photo">Upload file:</label><br>
     <input type="file" id="photo" name="photo" required><br><br>
