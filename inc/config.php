48810be4c865f2b38fda5bb25b8953ccf6d9a66f
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-16 09:24:17 +0300

    add config

---
+++ inc/config.php
@@ -0,0 +1,13 @@
+<?php
+const SITE_NAME = 'PhotoSwipe';
+const FOOTER_TEXT = 'PhotoSwipe Project, October 2024. All rights reserved.';
+const PAGES =  [
+    [
+        'name' => 'HOME',
+        'action' => 'views',
+    ],
+    [
+        'name' => 'FORM FILE',
+        'action' => 'form',
+    ],
+];
