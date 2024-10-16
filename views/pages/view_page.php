e164e035935dc441725aaae6db487bffd186470f
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-16 13:57:18 +0300

    view_page init photo end

--- views/pages/view_page.php
+++ views/pages/view_page.php
@@ -1,33 +1,44 @@
 <?php
+$dataSource = readImages();
 ?>
 
 <div class="pswp-gallery pswp-gallery--single-column" id="gallery--with-custom-button">
-    <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-2500.jpg"
-       data-pswp-width="1669"
-       data-pswp-height="2500"
-       target="_blank">
-        <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg" alt="" />
-    </a>
-    <!-- cropped thumbnail: -->
-    <a href="../images/image1.png"
-       data-pswp-width="1875"
-       data-pswp-height="2500"
-       data-cropped="true"
-       target="_blank">
-        <img src="../images/image1.png" alt="" />
-    </a>
-    <!-- data-pswp-src with custom URL in href -->
-    <a href="https://unsplash.com"
-       data-pswp-src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg"
-       data-pswp-width="2500"
-       data-pswp-height="1666"
-       target="_blank">
-        <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg" alt="" />
-    </a>
-    <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/6/img-2500.jpg"
-       data-pswp-width="2500"
-       data-pswp-height="1667"
-       target="_blank">
-        <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/6/img-200.jpg" alt="" />
-    </a>
+    <?php if (count($dataSource) > 0) :?>
+
+        <?php foreach ($dataSource as $source) : ?>
+            <a href="<?= $source['src']?>"
+               data-pswp-width="<?= $source['width']?>"
+               data-pswp-height="<?= $source['height']?>"
+               target="_blank">
+                <img src="<?= $source['src']?>" alt="<?= $source['alt']?>" />
+            </a>
+        <?php endforeach;?>
+    <?php endif;?>
 </div>
+
+<script type="module">
+import PhotoSwipeLightbox from '/photoswipe/photoswipe-lightbox.esm.js';
+const lightbox = new PhotoSwipeLightbox({
+    gallery: '#gallery--with-custom-button',
+    children: 'a',
+    pswpModule: () => import('/photoswipe/photoswipe.esm.js'),
+    bgOpacity: 0.2,
+    spacing: 0.5, // 50% of viewport width
+});
+lightbox.on('uiRegister', function() {
+    lightbox.pswp.ui.registerElement({
+        name: 'test-button',
+        ariaLabel: 'Toggle zoom',
+        order: 9,
+        isButton: true,
+        html: 'Zoom',
+        onClick: (event, el) => {
+            if ( confirm('Do you want to toggle zoom?') ) {
+                lightbox.pswp.toggleZoom();
+            }
+        }
+    });
+});
+lightbox.init();
+</script>
+
