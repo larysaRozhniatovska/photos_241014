13bdac618bfa1cf4f3bd3f61b482346aa05ef1a3
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-16 10:01:18 +0300

    add templates/default_template

---
+++ views/templates/default_template.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Gallery</title>
+</head>
+<body>
+<?php include 'views/common/header.php';?>
+<?php include 'views/common/menu.php';?>
+<main>
+    <?php include_once 'views/pages/'. $page . '_page.php'?>
+</main>
+<?php include 'views/common/footer.php';?>
+</body>
+</html>
